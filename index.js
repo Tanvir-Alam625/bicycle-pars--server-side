36971946305b57aea993f6c3f73e7b429c5735dd
tanveer <mdtanviralamht731@gmail.com>
2022-05-24 22:49:52 +0600

    server running testing

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("BiCycle parts server is running");
+});
+app.listen(port, () => {
+  console.log("server running port number ", port);
+});
