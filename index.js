ffaea2e18e62d8067ea7bce3dd1e1cb00600d1f6
tanveer <mdtanviralamht731@gmail.com>
2022-05-25 02:43:03 +0600

    mongodb init and tools api added

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, LoggerLevel } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.jhwgt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const partsCollection = client.db("bicycleParts").collection("parts");
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = partsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    //
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("BiCycle parts server is running");
 });
