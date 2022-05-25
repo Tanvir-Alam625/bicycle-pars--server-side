6e88147cd4d84cbc25c905c5e6121bcda65c4bab
tanveer <mdtanviralamht731@gmail.com>
2022-05-25 22:42:36 +0600

    produt order post and patch api added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, LoggerLevel } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  LoggerLevel,
+  ObjectId,
+} = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,11 +24,47 @@ async function run() {
     await client.connect();
 
     const partsCollection = client.db("bicycleParts").collection("parts");
+    const reviewsCollection = client.db("bicycleParts").collection("reviews");
+    const ordersCollection = client.db("bicycleParts").collection("orders");
+    // load tools data
     app.get("/tools", async (req, res) => {
       const query = {};
       const cursor = partsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    // get reviews data api
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // get purchase data api
+    app.get("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+    // stored order data api
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+    // update parts available data api
+    app.patch("/purchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const available = req.body.available;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          available: available,
+        },
+      };
+      const update = await partsCollection.updateOne(filter, updateDoc);
+      res.send(update);
     });
   } finally {
     //
